fb29d31abbc407ac20b33cd4559cb7b0f5f6f017
HNS Trader <68029273+HNStrader@users.noreply.github.com>
2020-08-31 22:43:09 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
 
 
-	<body class="is-preload">
 
 		<!-- Wrapper -->
 			<div id="wrapper">
@@ -195,15 +194,6 @@ export default function Home() {
 
 			</div>
 
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
 
-	</body>
   )
 }

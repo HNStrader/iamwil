3194e21ef5c040511c586e408f491b4c872ad926
HNS Trader <68029273+HNStrader@users.noreply.github.com>
2020-08-31 21:51:50 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">I Am Wil</a>
         </h1>
 
         <p className={styles.description}>
